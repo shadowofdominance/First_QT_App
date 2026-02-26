2d4130d6b7b5783eb6d9b0ccab899969577b3433
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2026-02-26 19:44:47 +0530

    Initial commit

---
+++ build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Simple_App_FILE C:/Users/acd07/Documents/QT_Projects/Simple_App/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/Simple_App.exe)
+set(__QT_DEPLOY_TARGET_Simple_App_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Simple_App_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
